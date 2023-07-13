128d3717cc9847bd9399a0aae6c9246411351a95
Claudio <claudiocaporaso1998@gmail.com>
2023-07-13 16:22:56 +0200

    input and output2

---
+++ inputAndOutput2.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class inputAndOutput2 {
+    public static void main(String[] args){
+        System.out.print("Claudio ");
+        System.out.println("Caporaso");
+
+
+    }
+}
