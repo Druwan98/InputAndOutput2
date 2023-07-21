189fce5eb081affe06ebca6039633bcf113e14be
Claudio <claudiocaporaso1998@gmail.com>
2023-07-21 19:04:48 +0200

    Nome classe corretto con l'iniziale MAIUSCOLA

--- inputAndOutput2.java
+++ inputAndOutput2.java
@@ -1,6 +1,6 @@
-package exercises;
+package exercises.inputAndOutput2;
 
-public class inputAndOutput2 {
+public class InputAndOutput2 {
     public static void main(String[] args){
         System.out.print("Claudio ");
         System.out.println("Caporaso");
